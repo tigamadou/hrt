f1a7a803aed47223de1c73628899d11adac2d4e4
Amadou <amadou140589@gmail.com>
2020-05-24 12:50:29 +0100

    devise added with routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  get 'login', to: 'session#new' 
+  devise_for :users
+  
 end
