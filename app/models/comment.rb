99626b1c77211baf57b53e8fc0a028773a038781
Amadou <amadou140589@gmail.com>
2020-05-26 13:44:24 +0100

    comments and like tests added. Rubocup errors fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
-    validates :author_id,:post_id, :text, presence: true
-    scope :most_recent, -> { order(created_at: :desc) }
+  validates :author_id, :post_id, :text, presence: true
+  scope :most_recent, -> { order(created_at: :desc) }
 end
